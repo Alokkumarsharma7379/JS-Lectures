705b99d30f3c24ea212e4e4792479802d6d52223
Alokkumarsharma7379 <194058580+Alokkumarsharma7379@users.noreply.github.com>
2026-04-06 21:04:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "alokgoogle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 ---- not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "11112212322"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,10 @@
+let name = "Alok"
+let age = 18
+let isBoolean = false
+
+console.log(typeof age);
+console.log(typeof "Alok");
+console.log(typeof isBoolean);
+
+console.log(typeof undefined);    // undefined
+console.log(typeof null);        // object
