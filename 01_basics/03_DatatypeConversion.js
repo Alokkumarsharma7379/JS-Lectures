04ddc3afb202a9bb6d817a58dca39cd30b32d600
Alokkumarsharma7379 <194058580+Alokkumarsharma7379@users.noreply.github.com>
2026-04-07 05:24:51 +0000

    Datatype and Conversion

---
+++ 01_basics/03_DatatypeConversion.js
@@ -0,0 +1,20 @@
+let score = "33"
+console.log(typeof score)
+let inNumber = Number(score)
+console.log(typeof inNumber)
+
+let score1 = "33abc"
+console.log(typeof score1)
+let inNumber1 = Number(score1)
+console.log(typeof inNumber1)
+console.log(inNumber1)
+
+let score2 = null
+console.log(typeof score2)
+let inNumber2 = Number(score2)
+console.log(typeof inNumber2)
+console.log(inNumber2)
+
+let isLoggedin =1;
+let booleanisLogin = Boolean(isLoggedin)
+console.log(booleanisLogin)
