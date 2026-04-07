4c92cb144ddc60f01159ac1332cf30b3b26b9184
Alokkumarsharma7379 <194058580+Alokkumarsharma7379@users.noreply.github.com>
2026-04-07 05:42:00 +0000

    Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,20 @@
+let value = 3;
+let negValue = -value
+console.log(negValue)
+console.log()
+
+let str1 = "hello"
+let str2 = " Alok"
+let str3 = str1 + str2
+console.log(str3)
+console.log()
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log("1" + (2 + 2))
+console.log(1 + 2 + "2")
+console.log()
+
+console.log(true)
+console.log(+true)
